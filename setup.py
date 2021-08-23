f35bbf18b242f54a604a5d5906e14aa4a0e212b0
JAVIER RODRIGO LOPEZ MALONE <jlopezmalone@santandertecnologia.com.ar>
2021-08-23 14:11:39 -0300

    Setting workflow

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh1:
+    long_description = fh1.read()
+
+#with open("CHANGELOG.txt", "r", encoding="utf-8") as fh2:
+#    change_log = fh2.read()
+
+classifiers = [
+    'Programming Language :: Python :: 3.9'
+    'Intended Audience :: Education',
+    'License :: OSI Approved :: MIT License',
+]
+ 
+VERSION = '1.0.0'
+
+setup(name='sample-backend',
+    version=VERSION,
+    description='Sample backend for todo-list application',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #change_log = change_log,
+    url='https://github.com/malonejv/py-sample-backend',  
+    project_urls={
+        "Bug Tracker": "https://github.com/malonejv/py-sample-backend/issues",
+    },
+    author='Javier López Malone',
+    author_email='malonejv@gmail.com',
+    license='MIT', 
+    classifiers=classifiers,
+    keywords=['todolist','todo'], 
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.9",
+)
